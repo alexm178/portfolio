499ff1d337734a3c7c8636d49c2b26f849842562
Alexander Miller <alexander.miller178@gmail.com>
2018-07-07 22:32:09 -0700

    major refactor

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
 var i = 0;
 var txt = 'By Alexander Anthony'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
+var $window = $(window)
 
-
-var state = {
-  sm: () => {return $(window).width() < "768"},
-  blocks: [
-    $("#first-block"),
-    $("#second-block"),
-    $('#third-block')
-  ],
-  pairs: [
-    $("#first-pair"),
-    $("#third-block")
-  ],
-  headings: [
-    "first-heading",
-    "second-heading",
-  ],
-  text: [
-    "first-text",
-    "second-text",
-  ],
-  watching: 0,
-  page: 1
-}
 
 function typeWriter() {
   if (i < txt.length) {
@@ -55,95 +33,165 @@ function chevron() {
 }
 
 
-$(window).on("load", () => {
-  resize()
+
+
+$window.on("load", () => {
   $("#header-text").fadeIn(1500, "linear", () => {
     typeWriter()
   })
+  sizeBlocks()
+})
+
+$window.on("resize", () => {
+  sizeBlocks()
 })
 
 
-function resize() {
-  var secondText = '<div class="col-md-6 text" id="second-text"><p>Mastery of both front-end and back-end development means that I can deliver a custom product tailored to your needs, no matter how complex those may be.  User authentication, real-time updates using web-sockets, complex data pipelines, and secure payment solutions can all be added to your website!</p></div>';
-  var secondHeading = '<div class="col-md-6 text-center heading" id="second-heading"><div id="a-galaxy-of">A GALAXY OF</div><div id="possibilities">POSSIBILITIES</div></div>';
-  $("#second-block").html(() => {
-    if (state.sm()) {
-      return secondHeading + secondText
-    } else {
-      return secondText + secondHeading
-    }
-  });
+
+
+// $(window).on("scroll", () => {
+//     if (state.sm()) {
+//       if (isInViewport($(state.blocks[state.watching]))) {
+//         if ($(document.getElementById(state.headings[state.watching])).css("right") !== "0px") {
+//           animateBlocksSmall()
+//         }
+//       }
+//     } else {
+//       if (isInViewport($(state.pairs[state.watching]))) {
+//         animateBlocksLarge();
+//       }
+//     }
+// })
+
+// function isInViewport(element){
+//   var elementTop = element.offset().top;
+//   var elementBottom = elementTop + element.outerHeight();
+//   var viewportTop = $(window).scrollTop();
+//   var viewportBottom = viewportTop + $(window).height();
+//   return elementBottom < viewportBottom + 50;
+// };
+//
+// function animateBlocksSmall() {
+//   if (state.watching === 2) {
+//     animateLogoSection()
+//   } else {
+//     $(document.getElementById(state.headings[state.watching])).animate({
+//       right: "0px"
+//     })
+//     $(document.getElementById(state.text[state.watching])).animate({
+//       left: "0px"
+//     })
+//     if (state.watching < state.blocks.length -1) {
+//       state.watching++
+//     }
+//   }
+// }
+//
+// function animateBlocksLarge() {
+//   if (state.watching === 1) {
+//     animateLogoSection()
+//   } else {
+//     state.blocks[state.watching].animate({
+//       right: "0px"
+//     })
+//     state.blocks[state.watching + 1].animate({
+//       left: "0px"
+//     })
+//     state.watching++
+//   }
+// }
+//
+// function animateLogoSection() {
+//   $('.third-heading-item').each(function(i) {
+//     var item = $(this);
+//     setTimeout(function() {
+//       item.addClass('grow');
+//     }, i*500); // delay 100 ms
+//   });
+//   $(".icon").each(function(index) {
+//     $(this).delay(1500 + 200*index).fadeIn(500);
+//   });
+// }
+//
+// $(".chev-down").on("click", () => {
+//   $('html, body').animate({scrollTop: state.pairs[0].offset().top * state.page}, 1000);
+//   state.page++
+// })
+
+function sizeBlocks() {
+
+  var vph = $window.innerHeight()
+
+  var $blocks = $(".block")
+
+  $blocks.each(index => {
+    var $block = $($blocks[index]);
+    var blockHeight = $block.outerHeight();
+    $block.css({
+      "padding-top": (vph - blockHeight) / 2 + "px"
+    })
+  })
+
 }
 
-$(window).on("resize", () => {
-  resize()
+
+
+
+var headings = $(".heading");
+var texts = $(".text")
+
+var animations = {
+  0: false,
+  1: false,
+  2: false,
+}
+
+$window.on("scroll", () => {
+  var vph = $window.innerHeight()
+  var page = Math.floor($window.scrollTop() / vph - 1)
+  switch (page) {
+    case 0:
+      animateText(page);
+      break;
+    case 1:
+      animateText(page);
+      break;
+    case 2:
+      animateIcons();
+      break;
+  }
 })
 
-$(window).on("scroll", () => {
-    if (state.sm()) {
-      if (isInViewport($(state.blocks[state.watching]))) {
-        if ($(document.getElementById(state.headings[state.watching])).css("right") !== "0px") {
-          animateBlocksSmall()
-        }
-      }
+function animateText(page) {
+  if (!animations[page]) {
+    var percent;
+    if ($window.width() > 768) {
+      percent = "50%"
     } else {
-      if (isInViewport($(state.pairs[state.watching]))) {
-        animateBlocksLarge();
-      }
+      percent = "0%"
     }
-})
-
-function isInViewport(element){
-  var elementTop = element.offset().top;
-  var elementBottom = elementTop + element.outerHeight();
-  var viewportTop = $(window).scrollTop();
-  var viewportBottom = viewportTop + $(window).height();
-  return elementBottom < viewportBottom + 50;
-};
-
-function animateBlocksSmall() {
-  if (state.watching === 2) {
-    animateLogoSection()
-  } else {
-    $(document.getElementById(state.headings[state.watching])).animate({
-      right: "0px"
+    $(headings[page * 2]).animate({
+      right: percent
+    }, 200, () => {
+      $(headings[page * 2 + 1]).animate({
+        left: percent
+      }, () => {
+        $(texts[page]).animate({
+          opacity: 1
+        }, 1000)
+      })
     })
-    $(document.getElementById(state.text[state.watching])).animate({
-      left: "0px"
-    })
-    if (state.watching < state.blocks.length -1) {
-      state.watching++
-    }
   }
 }
 
-function animateBlocksLarge() {
-  if (state.watching === 1) {
-    animateLogoSection()
-  } else {
-    state.blocks[state.watching].animate({
-      right: "0px"
-    })
-    state.blocks[state.watching + 1].animate({
-      left: "0px"
-    })
-    state.watching++
-  }
-}
-
-function animateLogoSection() {
+function animateIcons() {
   $('.third-heading-item').each(function(i) {
-    var item = $(this);
-    setTimeout(function() {
-      item.addClass('grow');
-    }, i*500); // delay 100 ms
-  });
-  $(".icon").each(function(index) {
-    $(this).delay(1500 + 200*index).fadeIn(500);
-  });
+      var item = $(this);
+      setTimeout(function() {
+        item.addClass('grow');
+      }, i*500); // delay 100 ms
+    });
+    $(".icon").each(function(index) {
+      $(this).delay(1500 + 200*index).fadeIn(500);
+    });
 }
-
-$(".chev-down").on("click", () => {
-  $('html, body').animate({scrollTop: state.pairs[0].offset().top * state.page}, 1000);
-  state.page++
-})
