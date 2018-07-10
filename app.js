24e0793859d1ce2618bfb00f46c85a3e503f36a5
Alexander Miller <alexander.miller178@gmail.com>
2018-07-10 08:41:43 -0700

    add public folder

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-var express - require(express);
+var express = require("express");
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+
 app.get("/", (req, res) => {
-  res.sendFile("./index.html")
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("listening")
 })
